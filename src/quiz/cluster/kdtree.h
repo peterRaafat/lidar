c205eefa11aec0de70fdf1319c000cfd0d39611e
peter <peter@DESKTOP-L5VFM64>
2021-02-20 16:42:57 +0200

    added kd_tree search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -75,9 +75,76 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		search(target, distanceTol, &root, 0, &ids);
 		return ids;
 	}
-	
+
+	void search(std::vector<float> target, float distanceTol, struct Node** pp_node ,int depth, std::vector<int>* p_result)
+	{
+		if (*(pp_node) != NULL)
+		{
+			if ((depth % 2) == 0)
+			{
+				//check by X
+				if (abs((*(pp_node))->point[0] - target[0]) <= distanceTol)
+				{
+					//point is in the box
+					float dist = sqrt((pow(((*(pp_node))->point[0] - target[0]), 2)) + (pow(((*(pp_node))->point[1] - target[1]), 2)));
+					if (dist <= distanceTol)
+					{
+						//add point to the vector
+						p_result->push_back((*(pp_node))->id);
+					}
+					search(target, distanceTol, &(*(pp_node))->left, ++depth, p_result);
+					search(target, distanceTol, &(*(pp_node))->right, ++depth, p_result);
+				}
+				else
+				{
+					//point is outside the box we need to check whether we split left or right
+					if ((*(pp_node))->point[0] > target[0])
+					{
+						//ignore the right section
+						search(target, distanceTol, &(*(pp_node))->left, ++depth, p_result);
+					}
+					else
+					{
+						//ignore left section
+						search(target, distanceTol, &(*(pp_node))->right, ++depth, p_result);
+					}
+				}
+			}
+			else
+			{
+				//check by Y
+				if (abs((*(pp_node))->point[1] - target[1]) <= distanceTol)
+				{
+					//point is in the box
+					float dist = sqrt((pow(((*(pp_node))->point[0] - target[0]), 2)) + (pow(((*(pp_node))->point[1] - target[1]), 2)));
+					if (dist <= distanceTol)
+					{
+						//add point to the vector
+						p_result->push_back((*(pp_node))->id);
+					}
+					search(target, distanceTol, &(*(pp_node))->left, ++depth, p_result);
+					search(target, distanceTol, &(*(pp_node))->right, ++depth, p_result);
+				}
+				else
+				{
+					//point is outside the box we need to check whether we split left or right
+					if ((*(pp_node))->point[1] > target[1])
+					{
+						//ignore the right section
+						search(target, distanceTol, &(*(pp_node))->left, ++depth, p_result);
+					}
+					else
+					{
+						//ignore left section
+						search(target, distanceTol, &(*(pp_node))->right, ++depth, p_result);
+					}
+				}
+			}
+		}
+	}
 
 };
 
